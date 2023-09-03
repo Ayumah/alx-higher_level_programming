124a3ec4cbdd86cbeac014a46ce2385574414f74
Ayumah <angieayuma@gmail.com>
2023-09-03 09:00:53 +0000

    Pycodestyle correction

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
-
-""" a function that computes the square value of all integers of a matrix """
-
 def square_matrix_simple(matrix=[]):
     new_matrix = matrix.copy()
 
     for i in range(len(matrix)):
         new_matrix[i] = list(map(lambda x: x**2, matrix[i]))
 
-    return new_matrix
+    return (new_matrix)
