3bc3a9a6a86430f1b5f2b9a64ebb971c312c767b
Ayumah <angieayuma@gmail.com>
2023-09-03 08:39:43 +0000

    Task 0: Squared simple

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,8 +2,7 @@
 
 """ a function that computes the square value of all integers of a matrix """
 
-def square_matrix_simple(matrix=[]):
-    
+def square_matrix_simple(matrix=[]): 
     new_matrix = matrix.copy()
 
     for i in range(len(matrix)):
