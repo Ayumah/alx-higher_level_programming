72f8b3599fd62c334722c4f1d23219dd55b54737
Ayumah <angieayuma@gmail.com>
2023-09-11 18:38:04 +0000

    Rectangle

--- 0x0A-python-inheritance/10-rectangle.py
+++ 0x0A-python-inheritance/10-rectangle.py
@@ -1,22 +1,15 @@
 #!/usr/bin/python3
-Rectangle = __import__('9-rectangle').Rectangle
-
 """
 Square module
 """
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """square class inherits from Rectangle"""
+    """class inherits from Rectangle"""
 
     def __init__(self, size):
-        """Method for initialized the attrubutes"""
-
-        super().__init__(size, size)
-        self.integer_validator("size", size)
+        """initialization method"""
+        self.integer_validator('size', size)
         self.__size = size
-
-    def area(self):
-        """rectangle area"""
-
-        return self.__size ** 2
+        super().__init__(self.__size, self.__size)
