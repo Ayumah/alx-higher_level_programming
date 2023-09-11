6e63d1b7e8f21ee340f56b1a7bf9f39d7dc96ad5
Ayumah <angieayuma@gmail.com>
2023-09-11 18:52:31 +0000

    Advanced Tasks

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+method module
+"""
+
+
+def add_attribute(obj, objname, value):
+    """add attribute to object
+    """
+    if hasattr(obj, "__dict__") == False:
+        raise TypeError("can't add new attribute")
+    setattr(obj, objname, value)
