df7fa3cde1cc78bdc3a4df1d88b00781df215ef6
Ayumah <angieayuma@gmail.com>
2023-09-13 03:15:35 +0000

    Mandatory tasks

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
-def write_file(filename="", text=""):
-    """ function that writes a string to a text file (UTF8)  """
+"""
+write_file is a function writes a string to a text file 
+(UTF8) and returns the number of characters written:
+"""
 
-    with open(filename, "w", encoding='utf-8') as f:
+
+def write_file(filename="", text=""):
+    with open(filename, "w", encoding="UTF-8") as f:
         return f.write(text)
