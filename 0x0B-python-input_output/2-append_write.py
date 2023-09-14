63340544965683dd09961f1c8240e2961e5bee4f
Ayumah <angieayuma@gmail.com>
2023-09-14 09:14:31 +0000

    Append write

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+Module with function append_write
+"""
+
+
 def append_write(filename="", text=""):
     """ function that appends a string returns the number of characters """
 
