cbd80ce47219fb94fb52ce7796f25886ed1d1637
Ayumah <angieayuma@gmail.com>
2023-09-14 09:38:35 +0000

    save to JSON file

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -8,5 +8,8 @@ import json
 
 
 def load_from_json_file(filename):
+    """ function that creates an object from JSON file"""
+
+
     with open(filename, "r", encoding="UTF-8") as f:
         return json.load(f)
