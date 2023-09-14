7995e2a0f15bcb002e056a8f4ba34ded3c5729d7
Ayumah <angieayuma@gmail.com>
2023-09-14 07:43:31 +0000

    Read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,9 +4,10 @@ module with function read_line
 """
 
 def read_file(filename=""):
+
     """
-    read_line is a function a function that reads a text file
-    (UTF8) and prints it to stdout:
+    read_line is a function that reads a text file (UTF8)
+    and prints it to stdout:
     """
 
     with open(filename, "r", encoding="UTF-8") as f:
